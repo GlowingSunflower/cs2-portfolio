09e2190412f7521722da9bc3631cee3e43a406bf
jesay riie <jmbsoledad@brc.pshs.edu.ph>
2026-09-06 22:06:06 +0800

    Implement score checking and feedback logic

--- q1/SOLEDAD_score_checker.py
+++ q1/SOLEDAD_score_checker.py
@@ -1,1 +1,15 @@
-TEST
+# Ask the user to enter a student score.student_score = int(input("Please enter your score, dear user: "))
+student_score = int(input("Please enter your score, dear user: "))
+
+# Confirm that the score is within the allowed range of 0 to 100.
+if student_score < 0 or student_score > 100:
+    print("Invalid Score.")
+  
+elif student_score >= 90:
+    print("Your score is Outstanding!")
+elif student_score >= 80:
+    print("Your score is Very Satisfactory.")
+elif student_score >= 75:
+    print("Your score is Satisfactory.")
+else:
+    print("Needs Improvement")
